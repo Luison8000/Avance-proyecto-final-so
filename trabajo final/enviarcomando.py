e42bf305a8d65e0f17fcaf4027dc91fd63363d93
Luison8000 <116618951+Luison8000@users.noreply.github.com>
2026-02-24 18:35:03 -0600

    Update enviarcomando.py

--- trabajo final/enviarcomando.py
+++ trabajo final/enviarcomando.py
@@ -1,8 +1,15 @@
-contexto = ssl.create_default_context()
-contexto.check_hostname = False
-contexto.verify_mode = ssl.CERT_NONE
+def enviar_comando(comando):
+    contexto = ssl.create_default_context()
+    contexto.check_hostname = False
+    contexto.verify_mode = ssl.CERT_NONE
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((HOST, PORT))
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((HOST, PORT))
 
-cliente = contexto.wrap_socket(cliente, server_hostname=HOST)
+    cliente = contexto.wrap_socket(cliente, server_hostname=HOST)
+
+    cliente.send(comando.encode())
+    respuesta = cliente.recv(4096).decode()
+
+    cliente.close()
+    return respuesta
